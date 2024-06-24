8415c656c33544c1a2348f0dfe539aaad403461d
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2024-06-25 00:43:51 +0200

    Update release-module.sh to v0.8.0

--- release-module.sh
+++ release-module.sh
@@ -2,14 +2,14 @@
 #
 # This file is part of Opal and has been released into the public domain.
 # SPDX-License-Identifier: CC0-1.0
-# SPDX-FileCopyrightText: 2021-2022 Mirian Margiani
+# SPDX-FileCopyrightText: 2021-2024 Mirian Margiani
 #
 # See https://github.com/Pretty-SFOS/opal/blob/main/opal-development/opal-release-module.md
 # for documentation.
 #
-# @@@ keep this line: based on template v0.6.1
+# @@@ keep this line: based on template v0.8.0
 #
-c__FOR_RELEASE_LIB__="0.6.3"
+c__FOR_RELEASE_LIB__="0.8.0"
 
 # Run this script from the module's root directory.
 source ../opal/opal-development/opal-release-module.sh
@@ -17,44 +17,38 @@ parse_arguments "$@"
 
 # Copy and edit this template file for new Opal modules.
 # Module metadata will be read from doc/module.opal.
-# Note: modules requiring extra build steps (Qt plugins) are not yet supported.
 
-# which files and directories to translate
-# Note: translations are built from the original sources, independent of the
-# files marked for distribution in copy_files.
-cTRANSLATE=(Opal)
+# Important directories:
+#
+# .                     - root directory of the module
+# ├── build             - (generated) build output directory
+# ├── build-doc         - (generated) documentation output directory
+# ├── doc               - module metadata and documentation
+# ├── Opal              - QML module root
+# │   └── MyModule      - public QML files
+# │       └── private   - private QML files
+# └── src               - C++ source root
+#     └── mymodule      - public C++ source and header files
+#         └── private   - private C++ files
 
-# un-comment the following line to initially setup translations
-# setup_translations
+# Define in cTRANSLATE which files and directories must be scanned for
+# translatable string. Note: translations are built from the original sources,
+# independent of the files marked for distribution in copy_files.
+cTRANSLATE=(Opal)  # options: "Opal" and/or "src"
 
-# Edit the copy_files() function if any additional copy steps are necessary.
-# By default, the main Opal directory will be copied with all contents. It might
-# be necessary to exclude certain files that are not meant for distribution.
-# Use BUILD_ROOT, QML_BASE, and DOC_BASE (below BUILD_ROOT) to define target paths.
+# Edit the copy_files() function if any additional copy steps are necessary. By
+# default, documentation is generated, and QML and C++ sources will be copied. It
+# might be necessary to exclude certain files that are not meant for distribution.
+# Use BUILD_ROOT, QML_BASE, SRC_BASE, and DOC_BASE (below BUILD_ROOT) to define
+# target paths.
+#
+# Note: put additional strings to be translated in the special file
+# "Opal/<YourModule>/private/ExtraTranslations.qml". This is a dummy file that
+# will not be included in the tarball but will be used as source for translations.
 function copy_files() {
     build_qdoc to="$DOC_BASE"
-    cp -r Opal "$QML_BASE/Opal" || return 1
-
-    # We don't have to distribute the extra translations dummy file, as translations
-    # are built separately and are merged if needed.
-    rm "$QML_BASE/Opal/Delegates/private/ExtraTranslations.qml"
-
-    # Generate a default attribution file that can be directly used in projects.
-    # No documentation will be generate for this file because it only exists in
-    # the release bundle.
-    printf "%s\n" \
-        "//@ This file is part of ${cMETADATA[fullName]}." \
-        "//@ https://github.com/Pretty-SFOS/${cMETADATA[fullName]}" \
-        "//@ SPDX-FileCopyrightText: ${cMETADATA[attribution]}" \
-        "//@ SPDX-License-Identifier: ${cMETADATA[mainLicenseSpdx]}" \
-        "" \
-        "Attribution {" \
-        "    name: \"${cMETADATA[fullNameStyled]} (v${cMETADATA[version]})\"" \
-        "    entries: \"${cMETADATA[attribution]}\"" \
-        "    licenses: License { spdxId: \"${cMETADATA[mainLicenseSpdx]}\"}" \
-        "    sources: \"https://github.com/Pretty-SFOS/${cMETADATA[fullName]}\"" \
-        "    homepage: \"https://github.com/Pretty-SFOS/opal\"" \
-        "}" "" > "$QML_BASE/Opal/Delegates/OpalDelegatesAttribution.qml"
+    [[ -d Opal ]] && { cp -r Opal --target-directory="$QML_BASE"    || return 1; } || true
+    [[ -d src  ]] && { cp -r src  --no-target-directory "$SRC_BASE" || return 1; } || true
 }
 
 # build the bundle
